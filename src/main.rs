809980ef668a50a7db743a009aae2903aad61094
Yao Yuan <yaoyuan@thoughtworks.com>
2018-06-06 22:20:18 +0200

    implemented median method and a wrapper struct to ensure no NaN can be given

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,6 @@ mod mean_median_mode;
 
 fn main() {
 	let list_of_nums = vec![21.2, 3.4, 0.0, 309.3];
-    println!("the result is {:?}", mean_median_mode::mean_median_mode(&list_of_nums));
+    println!("the mean is {:?}", mean_median_mode::get_mean(&list_of_nums));
+    println!("the median is {:?}", mean_median_mode::get_median(&list_of_nums));
 }
--- src/mean_median_mode.rs
+++ src/mean_median_mode.rs
@@ -1,15 +1,54 @@
+use std::cmp::Ordering;
 
-pub fn mean_median_mode(list: &Vec<f32>) -> Vec<f32> {
-	let mut mean: f32 = 0.0;
-;
+pub fn get_mean(list: &Vec<f32>) -> Vec<f32> {
+	let mut sum: f32 = 0.0;
 
 	let mut results: Vec<f32> = Vec::new();
 
 	for num in list {
-		mean += num;
+		sum += num;
 	}
 
-	results.push(mean as f32/ list.len() as f32);
+	results.push(sum as f32/ list.len() as f32);
 	results
 }
 
+
+pub fn get_median(list: &Vec<f32>) -> f32 {
+
+	let mut v: Vec<_> = list.iter().map(|v| NonNan::new(*v).unwrap()).collect();
+	v.sort();
+	let sorted_list: Vec<f32> = v.iter().map(|v| v.0).collect();
+
+
+
+	if list.len() % 2 == 0 {
+		let middle_index = list.len() / 2 ;
+		(sorted_list[middle_index] + sorted_list[middle_index - 1]) / 2.0
+	} else {
+		let middle_index = (list.len() - 1) / 2;
+		sorted_list[middle_index]
+	}
+
+}
+
+#[derive(PartialEq,PartialOrd)]
+struct NonNan(f32);
+
+impl NonNan {
+    fn new(val: f32) -> Option<NonNan> {
+        if val.is_nan() {
+            None
+        } else {
+            Some(NonNan(val))
+        }
+    }
+}
+
+impl Eq for NonNan {}
+
+impl Ord for NonNan {
+    fn cmp(&self, other: &NonNan) -> Ordering {
+        self.partial_cmp(other).unwrap()
+    }
+}
